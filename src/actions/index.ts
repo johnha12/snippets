3c1f3143006392bda0aeeeff050d6299cba0c5bd
Jonathan <80421998+johnha12@users.noreply.github.com>
2026-02-03 16:20:43 -0800

    snippet code

---
+++ src/actions/index.ts
@@ -0,0 +1,66 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { db } from '@/db';
+
+export async function editSnippet(id: number, code: string) {
+  await db.snippet.update({
+    where: { id },
+    data: { code }
+  });
+
+  revalidatePath(`/snippets/${id}`);
+  redirect(`/snippets/${id}`);
+}
+
+export async function deleteSnippet(id: number){
+  await db.snippet.delete({
+    where: { id }
+  });
+
+  revalidatePath('/');
+  redirect('/');
+}
+
+export async function createSnippet(formState: {message: string}, formData: FormData) {
+
+  try {
+    // make sure user enter valid inputs
+    const title = formData.get('title');
+    const code = formData.get('code');
+
+    if (typeof title !== 'string' || title.length < 3) {
+      return {
+        message: 'title must be longer'
+      };
+    };
+    if (typeof code !== 'string' || code.length < 10) {
+      return {
+        message: 'code must be longer'
+      };
+    };
+
+    // create new record in database
+    await db.snippet.create({
+      data: { 
+        title,
+        code
+      }
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message
+      };
+    } else {
+      return {
+        message: 'Something went wrong...'
+      }
+    }
+  }
+
+  revalidatePath('/');
+  // redirect user back to root route
+  redirect('/')
+}
